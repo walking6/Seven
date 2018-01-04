9ff499524beeed913b8ffa2f0173e1001e02e98c
WIB <33075932+oscwib@users.noreply.github.com>
2018-01-04 08:02:56 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/1.7.14"
-  LA = "CHROMEOS\t1.7.14\tChrome_OS\t1"      
+  UA = "Line/7.14.0 iPAD5,1 10.2.0"
+  LA = "IOSIPAD\t7.14.0\tiPhoneOS\t10.12.0"      
 
   rev = 0
 
